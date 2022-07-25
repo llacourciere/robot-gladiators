488b6985aa2ec091c6b1c43878a1e89699490343
LaLacou <108777796+users.noreply.github.com>
2022-07-24 20:57:09 -0700

    added the option to skip or fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
@@ -9,6 +10,12 @@ var enemyAttack = 12;
 var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
+    console.log(promptFight);
+    //Start or Skip Fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+
+    
 
     //Subtract the value of "playerAttack from the value 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
     enemyHealth = enemyHealth - playerAttack;
@@ -33,11 +40,29 @@ var fight = function() {
     );
     //check player health
     if (playerHealth <= 0) {
-        window.alert(playerName + " has died!")
+        window.alert(playerName + " has died!");
     }
     else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
-    
+    }
+    //if player chooses to skip
+    else if (promptFight === "skip" || promptFight === "SKIP") {
+        //confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        //if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        //subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 2;
+        }
+        // if no (false), ask question again by running fight() again
+        else {
+            fight();
+        }
+    } else {
+        window.alert("You need to choose a valid option. Try again!");
+    }
 };
 fight();
