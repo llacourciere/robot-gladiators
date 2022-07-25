1a6d3849ca43e1cf58634ff176949c8c62a4aff6
LaLacou <108777796+users.noreply.github.com>
2022-07-25 13:17:39 -0700

    added robot name array and multiple fighters

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,19 @@
+//Game state
+// WIN  player beats all enemy robots
+    // *fight all enemy robots
+    // * beat all enemy robots
+//LOSE players health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = [ "Roborto" , "Amy Android" , "Robo Trumble" ];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
@@ -65,4 +71,6 @@ var fight = function() {
         window.alert("You need to choose a valid option. Try again!");
     }
 };
-fight();
+for(var i = 0; i <enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
